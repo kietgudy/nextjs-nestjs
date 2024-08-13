06c4e15596b55dd2571e74530bebc2f6cdd3be3a
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-08-14 01:46:14 +0700

    init project & config, connect to db

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+   <main>
+    <p>Hello world</p>
+    <div>
+      <Button>Click</Button>
+    </div>
+   </main>
+  );
+}
