ece6e550c8bd33712b3d06f2597b1bdd0fdb81e5
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-08-14 14:48:10 +0700

    update schemas users

--- backend/src/users/schemas/user.schema.ts
+++ backend/src/users/schemas/user.schema.ts
@@ -3,17 +3,41 @@ import { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
-@Schema()
+@Schema({ timestamps: true })
 export class User {
-  @Prop()
-  name: string;
+    @Prop()
+    name: string;
 
-  @Prop()
-  age: number;
+    @Prop()
+    email: string;
 
-  @Prop()
-  address: string;
+    @Prop()
+    password: string;
+
+    @Prop()
+    phone: string;
+
+    @Prop()
+    address: string;
+
+    @Prop()
+    image: string;
+
+    @Prop()
+    role: string;
+
+    @Prop()
+    accountType: string;
+
+    @Prop()
+    isActive: string;
+
+    @Prop()
+    codeId: string;
+
+    @Prop()
+    codeExpired: Date;
 
 }
 
-export const UserSchema = SchemaFactory.createForClass(User);       
+export const UserSchema = SchemaFactory.createForClass(User);
