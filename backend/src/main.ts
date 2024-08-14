1ad6b05c26a98dbea911b5f39b0f392883f09c1a
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-08-14 15:13:03 +0700

    config version api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get('PORT')
+  const port = configService.get('PORT');
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ['1', '2'],
+  });
   await app.listen(port);
 }
 bootstrap();
