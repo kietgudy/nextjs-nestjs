784f5bec54ec62f94095b9598db0a192b23e8cbd
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-08-19 00:32:40 +0700

    payload add info user

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -22,11 +22,16 @@ export class AuthService {
   async login(user: IUser) {
     const payload = { username: user.email, sub: user._id };
     return {
+      user: {
+        email: user.email,
+        _id: user._id,
+        name: user.name
+      },
       access_token: this.jwtService.sign(payload),
     };
   }
 
   async register(user: CreateAuthDto) {
-    return await this.usersService.register(user)
+    return await this.usersService.register(user);
   }
 }
