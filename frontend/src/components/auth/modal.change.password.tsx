b80fd92c29fa594516434f392ef0e62fed70bba7
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-08-20 16:50:46 +0700

    handle change password success

--- frontend/src/components/auth/modal.change.password.tsx
+++ frontend/src/components/auth/modal.change.password.tsx
@@ -1,194 +1,206 @@
-'use client'
+"use client";
 
 import { useHasMounted } from "@/utils/customHook";
 import { Button, Form, Input, Modal, notification, Steps } from "antd";
-import { SmileOutlined, SolutionOutlined, UserOutlined } from '@ant-design/icons';
-import { useEffect, useState } from "react";
+import {
+  SmileOutlined,
+  SolutionOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { useState } from "react";
 import { sendRequest } from "@/utils/api";
 
 const ModalChangePassword = (props: any) => {
-    const { isModalOpen, setIsModalOpen } = props;
-    const [current, setCurrent] = useState(0);
-    const [form] = Form.useForm();
-    const [userId, setUserId] = useState("");
+  const { isModalOpen, setIsModalOpen } = props;
+  const [current, setCurrent] = useState(0);
+  const [form] = Form.useForm();
+  const [userEmail, setUserEmail] = useState("");
 
-    const hasMounted = useHasMounted();
+  const hasMounted = useHasMounted();
 
+  if (!hasMounted) return <></>;
 
-    if (!hasMounted) return <></>;
+  const onFinishStepZero = async (values: any) => {
+    const { email } = values;
+    const res = await sendRequest<IBackendRes<any>>({
+      url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/retry-password`,
+      method: "POST",
+      body: {
+        email,
+      },
+    });
 
-    const onFinishStep0 = async (values: any) => {
-        const { email } = values;
-        const res = await sendRequest<IBackendRes<any>>({
-            url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/retry-active`,
-            method: "POST",
-            body: {
-                email
-            }
-        })
-
-        if (res?.data) {
-            setUserId(res?.data?._id)
-            setCurrent(1);
-        } else {
-            notification.error({
-                message: "Call APIs error",
-                description: res?.message
-            })
-        }
-
+    if (res?.data) {
+      setUserEmail(res?.data?.email);
+      setCurrent(1);
+    } else {
+      notification.error({
+        message: "Call APIs error",
+        description: res?.message,
+      });
     }
+  };
 
-    const onFinishStep1 = async (values: any) => {
-        const { code } = values;
-        const res = await sendRequest<IBackendRes<any>>({
-            url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/check-code`,
-            method: "POST",
-            body: {
-                code, _id: userId
-            }
-        })
-
-        if (res?.data) {
-            setCurrent(2);
-        } else {
-            notification.error({
-                message: "Call APIs error",
-                description: res?.message
-            })
-        }
-
+  const onFinishStepOne = async (values: any) => {
+    const { code, password, confirmPassword } = values;
+    if (password !== confirmPassword) {
+      notification.error({
+        message: "Invalid error",
+        description: "Mật khẩu và mật khẩu xác nhận phải chính xác",
+      });
+      return;
     }
-    return (
-        <>
-            <Modal
-                title="Quên mật khẩu"
-                open={isModalOpen}
-                onOk={() => setIsModalOpen(false)}
-                onCancel={() => setIsModalOpen(false)}
-                maskClosable={false}
-                footer={null}
+    const res = await sendRequest<IBackendRes<any>>({
+      url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/change-password`,
+      method: "POST",
+      body: {
+        code,
+        password,
+        confirmPassword,
+        email: userEmail,
+      },
+    });
+
+    if (res?.data) {
+      setCurrent(2);
+    } else {
+      notification.error({
+        message: "Code error",
+        description: "Mã code không hợp lệ hoặc đã hết hạn",
+      });
+    }
+  };
+  return (
+    <>
+      <Modal
+        title="Quên mật khẩu"
+        open={isModalOpen}
+        onOk={() => setIsModalOpen(false)}
+        onCancel={() => {
+          setIsModalOpen(false);
+          setCurrent(0);
+          setUserEmail("")
+          form.resetFields()
+        }}
+        maskClosable={false}
+        footer={null}
+      >
+        <Steps
+          current={current}
+          items={[
+            {
+              title: "Email",
+              icon: <UserOutlined />,
+            },
+            {
+              title: "Verification",
+              icon: <SolutionOutlined />,
+            },
+
+            {
+              title: "Done",
+              icon: <SmileOutlined />,
+            },
+          ]}
+        />
+        {current === 0 && (
+          <>
+            <div style={{ margin: "20px 0" }}>
+              <p>
+                Để thực hiện thay đổi mật khẩu, vui lòng nhập email tài khoản
+                của bạn.
+              </p>
+            </div>
+            <Form
+              name="change-password"
+              onFinish={onFinishStepZero}
+              autoComplete="off"
+              layout="vertical"
+              form={form}
             >
-                <Steps
-                    current={current}
-                    items={[
-                        {
-                            title: 'Email',
-                            // status: 'finish',
-                            icon: <UserOutlined />,
-                        },
-                        {
-                            title: 'Verification',
-                            // status: 'finish',
-                            icon: <SolutionOutlined />,
-                        },
+              <Form.Item label="" name="email">
+                <Input />
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </>
+        )}
 
-                        {
-                            title: 'Done',
-                            // status: 'wait',
-                            icon: <SmileOutlined />,
-                        },
-                    ]}
-                />
-                {current === 0 &&
-                    <>
+        {current === 1 && (
+          <>
+            <div style={{ margin: "20px 0" }}>
+              <p>Vui lòng thực hiện đổi mật khẩu</p>
+            </div>
 
-                        <div style={{ margin: "20px 0" }}>
-                            <p>Để thực hiện thay đổi mật khẩu, vui lòng nhập email tài khoản của bạn.</p>
-                        </div>
-                        <Form
-                            name="change-password"
-                            onFinish={onFinishStep0}
-                            autoComplete="off"
-                            layout='vertical'
-                            form={form}
-                        >
-                            <Form.Item
-                                label=""
-                                name="email"
-                            >
-                                <Input />
-                            </Form.Item>
-                            <Form.Item
-                            >
-                                <Button type="primary" htmlType="submit">
-                                    Submit
-                                </Button>
-                            </Form.Item>
-                        </Form>
-                    </>
-                }
+            <Form
+              name="change-pass-2"
+              onFinish={onFinishStepOne}
+              autoComplete="off"
+              layout="vertical"
+            >
+              <Form.Item
+                label="Code"
+                name="code"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your code!",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
 
-                {current === 1 &&
-                    <>
-                        <div style={{ margin: "20px 0" }}>
-                            <p>Vui lòng thực hiện đổi mật khẩu</p>
-                        </div>
+              <Form.Item
+                label="Mật khẩu mới"
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your new password!",
+                  },
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
 
-                        <Form
-                            name="change-pass-2"
-                            onFinish={onFinishStep1}
-                            autoComplete="off"
-                            layout='vertical'
+              <Form.Item
+                label="Xác nhận mật khẩu"
+                name="confirmPassword"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your new password!",
+                  },
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
 
-                        >
-                            <Form.Item
-                                label="Code"
-                                name="code"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Please input your code!',
-                                    },
-                                ]}
-                            >
-                                <Input />
-                            </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Confirm
+                </Button>
+              </Form.Item>
+            </Form>
+          </>
+        )}
 
-                            <Form.Item
-                                label="Mật khẩu mới"
-                                name="password"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Please input your new password!',
-                                    },
-                                ]}
-                            >
-                                <Input.Password />
-                            </Form.Item>
-
-                            <Form.Item
-                                label="Xác nhận mật khẩu"
-                                name="confirmPassword"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Please input your new password!',
-                                    },
-                                ]}
-                            >
-                                <Input.Password />
-                            </Form.Item>
-
-                            <Form.Item
-                            >
-                                <Button type="primary" htmlType="submit">
-                                    Confirm
-                                </Button>
-                            </Form.Item>
-                        </Form>
-                    </>
-                }
-
-                {current === 2 &&
-                    <div style={{ margin: "20px 0" }}>
-                        <p>Tải khoản của bạn đã được thay đổi mật khẩu thành công. Vui lòng đăng nhập lại</p>
-                    </div>
-                }
-            </Modal>
-        </>
-    )
-}
+        {current === 2 && (
+          <div style={{ margin: "20px 0" }}>
+            <p>
+              Tải khoản của bạn đã được thay đổi mật khẩu thành công. Vui lòng
+              đăng nhập lại
+            </p>
+          </div>
+        )}
+      </Modal>
+    </>
+  );
+};
 
 export default ModalChangePassword;
